0463e256edfbaa2f64d6b107e6b7d876bca8ad46
LucasMRC <lucas.reynacordoba22@gmail.com>
2019-03-16 16:14:21 -0300

    Add logout login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const { errorHandler } = require('../middleware');
-const { postRegister } = require('../controllers');
+const { postRegister, postLogin, getLogout } = require('../controllers');
 
 /* GET User Index */
 router.get('/', (req, res, next) => {
@@ -23,20 +22,12 @@ router.get('/login', (req, res, next) => {
 });
 
 /* POST /login */
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login'
-  })
-);
+
+router.post('/login', postLogin);
 
 /*  GET /logout */
 
-router.get('/logout', (req, res, next) => {
-  req.logout();
-  res.redirect('/');
-});
+router.get('/logout', getLogout);
 
 /* GET /profile */
 router.get('/profile', (req, res, next) => {
