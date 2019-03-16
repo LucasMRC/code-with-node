5569617406735af5c398d556090c11bd5ac46109
LucasMRC <lucas.reynacordoba22@gmail.com>
2019-03-16 15:36:24 -0300

    Add login/logout logic

--- app.js
+++ app.js
@@ -52,6 +52,9 @@ app.use(
     saveUninitialized: true
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
